27b49611a2d5888321d19292dbb6ca1d60ddff56
Diego Andres Rojas <diegoandresrojas2000@gmail.com>
2018-01-31 15:38:05 -0500

    Project portfolio send to github

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// Contact Form
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var form = document.getElementById('needs-validation');
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  }, false);
+})();
+//sticky animation
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({
+      scrollTop: destino.offset().top
+    }, 1200);
+    return false;
+  });
+});
